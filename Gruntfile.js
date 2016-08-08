7c4f78f69aff13c39410df583ea66daad1dfe5bd
John Hancock <jhancock1975@gmail.com>
2016-08-07 21:06:09 -0400

    trying grunt nodemone

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,36 @@ module.exports = function (grunt) {
         configDir: require('path').resolve('tasks')
     });
 
-    
-        grunt.loadNpmTasks('grunt-makara-amdify');
+   grunt.initConfig({
+    env: {
+      dev: {
+        NODE_ENV: 'development'
+      },
+      test: {
+        NODE_ENV: 'test'
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          ext: 'js,html',
+          watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('default', ['env:dev']);    
+  grunt.loadNpmTasks('grunt-makara-amdify');
     
     // Register group tasks
-    grunt.registerTask('build', ['eslint', 'eslint', 'dustjs', 'less', 'requirejs', 'copyto']);
+  grunt.registerTask('build', ['eslint', 'eslint', 'dustjs', 'less', 'requirejs', 'copyto']);
 
-    grunt.registerTask('test', [ 'eslint', 'mochacli' ]);
+  grunt.registerTask('test', [ 'eslint', 'mochacli' ]);
 
     
 };
