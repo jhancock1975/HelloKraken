5a53abbef6209a18427361da9ed2722162eb17be
John Hancock <jhancock1975@gmail.com>
2016-08-07 21:15:39 -0400

    roll back changes the bad way

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,36 +8,13 @@ module.exports = function (grunt) {
         configDir: require('path').resolve('tasks')
     });
 
-   grunt.initConfig({
-    env: {
-      dev: {
-        NODE_ENV: 'development'
-      },
-      test: {
-        NODE_ENV: 'test'
-      }
-    },
-    nodemon: {
-      dev: {
-        script: 'server.js',
-        options: {
-          ext: 'js,html',
-          watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-env');
-  grunt.loadNpmTasks('grunt-nodemon');
-
-  grunt.registerTask('default', ['env:dev']);    
-  grunt.loadNpmTasks('grunt-makara-amdify');
+    
+        grunt.loadNpmTasks('grunt-makara-amdify');
     
     // Register group tasks
-  grunt.registerTask('build', ['eslint', 'eslint', 'dustjs', 'less', 'requirejs', 'copyto']);
+    grunt.registerTask('build', ['eslint', 'eslint', 'dustjs', 'less', 'requirejs', 'copyto']);
 
-  grunt.registerTask('test', [ 'eslint', 'mochacli' ]);
+    grunt.registerTask('test', [ 'eslint', 'mochacli' ]);
 
     
 };
